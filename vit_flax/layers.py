f447accd8c7dee1b4aaee8eab8978199713f41d6
vballoli <balloli.vb@gmail.com>
2020-10-05 19:01:56 +0200

    Layers refactor

--- vit_flax/layers.py
+++ vit_flax/layers.py
@@ -1,36 +1,37 @@
-import jax
-from jax import lax
-from jax import random
-import jax.numpy as jnp
-
-import flax
 from flax import nn
 
 
 class FeedForward(nn.Module):
-  """ """
+  """Simple FeedForward module
+
+  x -> Dense(x, latent_dim) -> GeLU -> Dense(x, original_dim)
+  """
   def apply(self, x, latent_dim):
     dim = x.shape[-1]
-    x = nn.Dense(x, latent_dim, name='fc1')
+    x = nn.Dense(x, latent_dim)
     x = nn.gelu(x)
-    x = nn.Dense(x, dim, name='fc2')
+    x = nn.Dense(x, dim)
     return x
 
 
 class Residual(nn.Module):
-  """ """
+  """Simple residual function
+
+  x -> fn(x) + x"""
   def apply(self, x, residual_fn):
     return residual_fn(x) + x
 
 
 class PreNorm(nn.Module):
-  """ """
+  """Apply function to norm
+  """
   def apply(self, x, norm, fn):
     return fn(norm(x))
-      
+
 
 class Transformer(nn.Module):
-  """ """
+  """Simple implementation of a Transformer
+  """
   def apply(self, x, depth, num_heads, feed_forward_dim_1):
     attention = nn.SelfAttention.partial(num_heads=num_heads)
     norm = nn.LayerNorm
