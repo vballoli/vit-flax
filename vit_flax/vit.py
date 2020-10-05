c3e07f6af0ca066337061e6e8f83c155f424512d
vballoli <balloli.vb@gmail.com>
2020-10-05 19:03:12 +0200

    Minor ViT changes

--- vit_flax/vit.py
+++ vit_flax/vit.py
@@ -1,26 +1,26 @@
-from vit_flax.layers import Transformer, FeedForward
+from vit_flax.layers import Transformer
 
-import jax
-from jax import lax
-from jax import random
-import jax.numpy as jnp
-
-import flax
 from flax import nn
+from jax import lax
 
 
 class ViT(nn.Module):
   """ """
-  def apply(self, x, patch_size, dim, depth, num_heads, dense_dims, img_size, num_classes):
+  def apply(self, x,
+            patch_size, dim, depth, num_heads, dense_dims,
+            img_size, num_classes,
+            initializer=nn.initializers.normal(stddev=1.0)):
     b, h, w, c = x.shape
     patch = x.reshape(b, c, h, w)
     num_patches = (img_size // patch_size) ** 2
-    patch = patch.reshape(b, int((h*w)/(patch_size*patch_size)), int(patch_size*patch_size*c))
-    embedding_dim = c * patch_size ** 2
+    patch = patch.reshape(b, (h*w)//(patch_size*patch_size), c*patch_size**2)
+
     fc_embedding = nn.Dense(patch, dim)
 
-    class_tokens = self.param('class_tokens', (b, 1, dim), nn.initializers.normal(stddev=1.0))
-    pos_embedding = self.param('pos_embedding', (1, num_patches+1, dim), nn.initializers.normal(stddev=(1.0)))
+    class_tokens = self.param('class_tokens', (b, 1, dim), initializer)
+    pos_embedding = self.param(
+      'pos_embedding', (1, num_patches+1, dim), initializer
+    )
     x = lax.concatenate([class_tokens, fc_embedding], dimension=1)
     x += pos_embedding
     x = Transformer(x, depth, num_heads, dense_dims[0])
